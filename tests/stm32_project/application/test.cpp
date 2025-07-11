8245e022744d342d4902d979a95f94c8131d42f6
Florin Tobler <florin.tobler@hotmail.com>
2025-07-11 18:45:20 +0200

    add stm32 test project

---
+++ tests/stm32_project/application/test.cpp
@@ -0,0 +1,18 @@
+/*
+ * test.cpp
+ *
+ *  Created on: Jul 11, 2025
+ *      Author: ftobler
+ */
+
+#include "test.h"
+
+
+
+
+
+void test_fn() {
+
+}
+
+
---
+++ tests/stm32_project/application/test.h
@@ -0,0 +1,25 @@
+/*
+ * test.h
+ *
+ *  Created on: Jul 11, 2025
+ *      Author: ftobler
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+void test_fn();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* TEST_H_ */
